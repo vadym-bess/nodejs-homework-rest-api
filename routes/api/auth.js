f6c431c4849b8feec5ec6131ab5d920f15033353
Vadym <besarabvadim2@gmail.com>
2023-04-03 20:40:20 +0300

    avatar commit

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,12 @@
 const express = require("express");
-const { register, login, logout, current } = require("../../controllers/auth");
-const { userRegisterValidation } = require("../../middlewares");
+const {
+  register,
+  login,
+  logout,
+  current,
+  avatars,
+} = require("../../controllers/auth");
+const { userRegisterValidation, upload } = require("../../middlewares");
 const { authMiddleware } = require("../../middlewares/authMiddleware");
 
 const router = express.Router();
@@ -9,5 +15,6 @@ router.post("/register", userRegisterValidation, register);
 router.post("/login", userRegisterValidation, login);
 router.post("/logout", authMiddleware, logout);
 router.post("/current", authMiddleware, current);
+router.patch("/avatars", authMiddleware, upload.single("avatars"), avatars);
 
 module.exports = { authRouter: router };
